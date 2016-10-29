f379170eed1351e4432c723544ad73ab04d1d4d5
dmoeller <dmoeller@marketvolt.info>
2016-10-28 20:50:23 -0500

    Initial Commit

---
+++ Libraries/Game/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Game
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public Color? Color { get; set; }
+        public string Name { get; set; }
+
+        public Player(int id)
+        {
+            this.Id = id;
+            this.Color = null;
+            this.Name = id.ToString();
+        }
+
+        public Player(string name)
+        {
+            this.Name = name;
+            int result;
+            if (int.TryParse(name, out result))
+                this.Id = result;
+        }
+
+        public Player(int id, string name, Color? color =null)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Color = color;
+        }
+    }
+}
---
+++ Libraries/Game/PlayingSurface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Game
+{
+    class PlayingSurface
+    {        
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public Point Position { get; set; }
+
+        public Point Center { get { return new Point(Position.X + Width / 2, Position.Y + Height / 2); } }
+    }
+}
